969858af1f2d102437b037d1f2471461004ebdb2
Mike Moran <mike@houseofmoran.com>
2017-01-03 17:45:08 +0100

    switch to madrid instead of london

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-var location1 = [51.505, -0.09];
+// var location1 = [51.505, -0.09];
+var location1 = [40.415363, -3.707398];
 var location2 = [40.730610, -73.935242];
 
-var map1 = L.map('map1').setView(location1, 9);
-var map2 = L.map('map2').setView(location2, 9);
+var map1 = L.map('map1').setView(location1, 6);
+var map2 = L.map('map2').setView(location2, 6);
 
 var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
