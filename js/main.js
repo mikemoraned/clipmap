21f4c2011642ba9d2f41756794b013b809feae8d
Mike Moran <mike@houseofmoran.com>
2017-01-03 18:13:56 +0100

    add markers for all destinations

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 // var location1 = [51.505, -0.09];
 var location1 = [40.415363, -3.707398];
 var location2 = [40.730610, -73.935242];
+var destinations = [
+    location1,
+    [38.919144, -6.340805],
+    [43.312691, -1.993332],
+    [41.390205, 2.154007],
+    [42.878212, -8.544844],
+    [43.262985, -2.935013],
+    [40.426044, -3.565165],
+    [40.942902, -4.108807],
+    [37.339676, -5.841805],
+    [43.362343, -8.411540],
+    [40.304665, -3.723679],
+];
+
+function addAll(map, origin, destinations) {
+    L.marker(origin).addTo(map);
+    L.Polyline.Arc(origin, destinations[0]).addTo(map);
+    destinations.forEach(function(destination){
+        L.marker(destination).addTo(map);
+    });
+}
 
 var map1 = L.map('map1').setView(location1, 6);
 var map2 = L.map('map2').setView(location2, 6);
@@ -12,15 +33,14 @@ L.tileLayer(osmUrl, {
     attribution: osmAttrib,
     minZoom: 4, maxZoom: 12,
 }).addTo(map1);
-var marker1 = L.marker(location1).addTo(map1);
-L.Polyline.Arc(location1, location2).addTo(map1);
+addAll(map1, location2, destinations);
 
 L.tileLayer(osmUrl, {
     attribution: osmAttrib,
     minZoom: 4, maxZoom: 12,
 }).addTo(map2);
-var marker2 = L.marker(location2).addTo(map2);
-L.Polyline.Arc(location1, location2).addTo(map2);
+addAll(map2, location2, destinations);
+
 map2.on('zoomend', function() {
     map1.setZoom(map2.getZoom());
 });
